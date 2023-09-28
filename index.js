6735068c32c8c325b458a9abafd99bdcc5440a79
anemc19 <anementxakacrespo@gmail.com>
2023-09-28 11:47:26 +0200

    add/ejercicio

---
+++ index.js
@@ -0,0 +1,26 @@
+/* Problema 1 */
+
+let contador = 0;
+
+function letrasFrase(frase, letra) {
+
+  for (let i = 0; i < frase.length; i++) {
+
+    if (frase[i] === letra) {
+      
+    }
+  }
+
+  return contador
+
+}
+
+console.log(letrasFrase)
+
+/* Problema 2 */
+
+let numero = prompt("Indica un número");
+
+if (numero % 2 !== 0) {
+    return ()
+}
